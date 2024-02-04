e9eb4c602dd554f45f6485d991ed5239b5fc494a
Marianna Fernandes <145359517+mariannalf@users.noreply.github.com>
2024-02-04 11:00:09 -0600

    Add project files.

---
+++ CourseRegistration/wwwroot/js/site.js
@@ -0,0 +1,116 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function deleteCourse(courseId) {
+    if (confirm("Are you sure you want to delete this course?")) {
+        $.ajax({
+            url: '/Courses/' + courseId,
+            type: 'DELETE',
+            success: function (result) {
+                window.location.href = "/Courses";
+            }
+        });
+    }
+}
+
+function deleteInstructor(instructorId) {
+    if (confirm("Are you sure you want to delete this instructor?")) {
+        $.ajax({
+            url: '/Instructors/' + instructorId,
+            type: 'DELETE',
+            success: function (result) {
+                window.location.href = "/Instructors";
+            }
+        });
+    }
+}
+
+function deleteStudent(studentId) {
+    if (confirm("Are you sure you want to delete this student?")) {
+        $.ajax({
+            url: '/Students/' + studentId,
+            type: 'DELETE',
+            success: function (result) {
+                window.location.href = "/Students";
+            }
+        });
+    }
+}
+
+
+
+function editCourse(courseId, courseNumber, name, description) {
+    document.getElementById("label").innerText = "Edit Course";
+
+    document.getElementById("courseId").value = courseId;
+    document.getElementById("courseNumber").value = courseNumber;
+    document.getElementById("name").value = name;
+    document.getElementById("description").value = description;
+
+    document.getElementById("submit").innerText = "Update";
+    $("#modal").modal("show");
+}
+
+function newCourse() {
+    document.getElementById("label").innerText = "Add Course";
+    document.getElementById("courseId").value = "";
+    document.getElementById("courseNumber").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("description").value = "";
+
+    document.getElementById("submit").innerText = "Create";
+
+    $("#modal").modal("show");
+}
+
+
+function editInstructor(instructorId, firstName, lastName, email, courseId) {
+    document.getElementById("label").innerText = "Edit Instructor";
+
+    document.getElementById("instructorId").value = instructorId;
+    document.getElementById("firstName").value = firstName;
+    document.getElementById("lastName").value = lastName;
+    document.getElementById("email").value = email;
+    document.getElementById("courseId").value = courseId;
+    document.getElementById("submit").innerText = "Update";
+    $("#modal").modal("show");
+}
+
+function newInstructor() {
+    document.getElementById("label").innerText = "Add Instructor";
+    document.getElementById("instructorId").value = "";
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("courseId").value = "";
+    document.getElementById("submit").innerText = "Create";
+
+    $("#modal").modal("show");
+}
+
+
+function editStudent(studentId, firstName, lastName, email, phoneNumber) {
+    document.getElementById("label").innerText = "Edit Student";
+
+    document.getElementById("studentId").value = studentId;
+    document.getElementById("firstName").value = firstName;
+    document.getElementById("lastName").value = lastName;
+    document.getElementById("email").value = email;
+    document.getElementById("phoneNumber").value = phoneNumber;
+    document.getElementById("submit").innerText = "Update";
+    $("#modal").modal("show");
+}
+
+function newStudent() {
+    document.getElementById("label").innerText = "Add Student";
+    document.getElementById("studentId").value = "";
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phoneNumber").value = "";
+    document.getElementById("submit").innerText = "Create";
+
+    $("#modal").modal("show");
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+Assignment 2
+============
+
+   > **Task:** Submit to complete this assignment  
+   > Overdue - January 21 at 11:59 PM  
+   > 30 Marks
+
+For this assignment, re-visit the Course Registration website you created in Assignment 1.
+
+You must enhance the website to include forms that allow you to create and edit students, instructors, and courses. The Student entry page should also allow students to register for existing courses. The entry forms can be placed on the same screen as the existing pages that show the list of students, instructors, and courses, or they can be added on new pages.
+
+Use Entity Framework Core migrations to take the classes for students, instructors, and courses and create a database. Since students can enroll in multiple courses and courses can hold multiple students, this is a many-to-many relationship. You'll need to define a class and table called StudentCourse that holds records linking students to courses. Use LINQ to generate a list of Students for each course and display it in the modal dialog you developed in Assignment 1.
+
+You do not need to add validation at this point to the front-end, but you should have validation on the back-end that prevents bad data from entering the database. Define a data access layer that can do validation and error-handling.
+
+Your application should also use DTOs between the data layer and the controller rather than passing around the actual database entity classes.
+
+
+_________________________________
+Instructions to run application:
+=================================
+Run Commands in the folder CourseRegistration.Data:
+>>  dotnet tool install --global dotnet-ef
+>>  dotnet ef database update
+
+
+
